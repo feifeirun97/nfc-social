73101a2abceb4b28bed88a31ba0537e1941568bc
feifeirun <61611167+feifeirun97@users.noreply.github.com>
2021-12-28 00:45:40 +1100

    add header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 
 import { AiFillWechat, AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 import copy from 'copy-to-clipboard';
-
+import './App.css'
 function App() {
 
   return (
-    <div style={{ height: '100vh', width: '100vw' }}>
-      <div style={{ margin: '10vh auto', width: '70%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-        <AiFillWechat style={{ fontSize: '150px', color: ' #7BB32E' }} onClick={
+    <div className="container" style={{ height: '100vh', width: '100vw' }}>
+
+      <div className="bakcground">
+        <div className="title1">Helloooo</div>
+        <div className="title2">Nice to meet you</div>
+      </div>
+
+      <div style={{ margin: '5vh auto', width: '50%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+        <AiFillWechat style={{ fontSize: '110px', color: ' #7BB32E' }} onClick={
           () => {
             if (copy("feifeirun88")) {
               console.log("复制成功");
@@ -17,8 +23,8 @@ function App() {
             window.location.href = 'weixin://';
           }} />
 
-        <AiFillLinkedin style={{ fontSize: '150px', color: '#0072B1' }} onClick={() => { window.location.href = 'https://www.linkedin.com/in/fei-xu-096b08215/'; }} />
-        <AiFillGithub style={{ fontSize: '150px', color: '#171515' }} onClick={() => { window.location.href = 'https://github.com/feifeirun97'; }} />
+        <AiFillLinkedin style={{ fontSize: '110px', color: '#0072B1' }} onClick={() => { window.location.href = 'https://www.linkedin.com/in/fei-xu-096b08215/'; }} />
+        <AiFillGithub style={{ fontSize: '110px', color: '#171515' }} onClick={() => { window.location.href = 'https://github.com/feifeirun97'; }} />
       </div>
     </div>
   );
